56e3c775803c6cd8632e8bc4c729862e7aabb874
Simon <simon.mylius.rasmussen@post.au.dk>
2019-05-26 20:00:37 +0200

    struktur for rapport

---
+++ rapport/rpm-detect.c
@@ -0,0 +1,69 @@
+﻿/*
+ * rpm_detect.c
+ *
+ *  Created on: 25. apr. 2019
+ *      Author: Søren Korsgaard
+ */
+
+#include "rpm_detect.h"
+#define TPM_CH 0    // TPM channel for PTE 20
+#define RPM_PIN 20  //PTE 20 is used to detect rpm
+
+enum {
+	wait, set_data
+} state;
+volatile int count, rpm;
+void TPM1_IRQHandler() {
+	static uint32_t overflows = 0;
+	static uint32_t prev_count = 0;
+	uint32_t timer_val;
+
+	if (TPM1->STATUS & TPM_STATUS_TOF_MASK) {
+		overflows++;
+	}
+
+	if (TPM1->STATUS & TPM_STATUS_CH0F_MASK) {
+		timer_val = TPM1->CONTROLS[TPM_CH].CnV;
+		//if we have a overflow then we have to add
+		timer_val |= (overflows << 16);
+		count = timer_val - prev_count;
+		prev_count = timer_val;
+		rpm = 60 * 375000 / count;
+		state = set_data;
+	}
+	// reset all flags
+	TPM1->STATUS |= TPM_STATUS_TOF_MASK | TPM_STATUS_CH0F_MASK
+			| TPM_STATUS_CH1F_MASK;
+}
+
+void init_read_rpm() {
+	//set clock
+	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	//set clock source to TPM
+	SIM->SOPT2 |= (SIM_SOPT2_TPMSRC(1) | SIM_SOPT2_PLLFLLSEL_MASK);
+	//load counter to max value 2^16
+	TPM1->MOD = 0xffff;
+	//set channel to input capture and enable intterupt
+	TPM1->CONTROLS[TPM_CH].CnSC = TPM_CnSC_ELSA_MASK | TPM_CnSC_CHIE_MASK;
+	//set pin to timer TPM
+	PORTE->PCR[RPM_PIN] |= (3UL << 8);
+	// Enable0 interrupts, with 128 prescaler. so count freq is 48MHz /128 = 375 kHz
+	TPM1->SC = TPM_SC_CMOD(1) | TPM_SC_PS(7) | TPM_SC_TOIE_MASK;
+
+	NVIC_SetPriority(TPM1_IRQn, 3);
+	NVIC_ClearPendingIRQ(TPM1_IRQn);
+	NVIC_EnableIRQ(TPM1_IRQn);
+
+}
+int get_rpm() {
+	static int data = 0;
+	if (state != set_data) {
+		printf("rpm is: %d \n", rpm);
+		state = wait;
+		data = rpm;
+	}
+
+	return data;
+}
+
---
+++ rapport/vin-vs-vout.m
@@ -0,0 +1,98 @@
+%% Spændingstab i den aktive ensretter
+close all; clear; clc
+
+% 1. måling
+data1 = load('delta_vout_fase1.mat');
+% X-akse fra .csv fil (tid [s])
+t1 = data1.deltavoutfase1.second;
+% Inputspænding fra .csv fil (Volt [V])
+vin = data1.deltavoutfase1.Volt1;
+% Outputspænding fra .csv fil (Volt [V])
+vout = data1.deltavoutfase1.Volt;
+% Antal samples i signalerne
+N1 = length(vin);
+n1 = 0:N1-1;
+% Varighed af signalerne
+Tdur1 = t1(2000)-t1(1);
+% Samplevarighed
+Ts1 = Tdur1/N1;
+% Samplingfrekvens
+fs1 = 1/Ts1;
+% Midling af input og output
+vin_mid = medfilt1(vin,200);
+vout_mid = medfilt1(vout,200);
+% Max værdi for vin og vout
+vin_mid_max = max(vin_mid);
+vout_mid_max = max(vout_mid);
+
+% Plot af hele sigalet
+figure
+subplot(211)
+plot(n1*Ts1,vin_mid,'b'), hold on
+plot(n1*Ts1,vout_mid,'r')
+xlabel('Tid, [s]'), ylabel('Fase 1 & Vout, [V]')
+legend('Fase 1','Vout'), grid
+title('Fase 1 vs. Vout, 1. måling')
+% Udvælgelse af segmenter, hvor Fase 1 > Vout
+vin_seg = vin_mid(924:1452);
+vout_seg = vout_mid(924:1452);
+% Plot af segmenter
+subplot(212)
+plot(n1(924:1452)*Ts1,vin_seg), hold on
+plot(n1(924:1452)*Ts1,vout_seg )
+xlabel('Tid, [s]'), ylabel('Fase 1 & Vout, [V]')
+legend('Fase 1','Vout'), grid
+title('Segment af Vin og Vout')
+% Udregning af spændingstabet, (Vin - Vout) 
+v_delta1 = mean(vin_seg)-mean(vout_seg);
+
+%% 2. måling
+%close all; clear; clc
+
+data2 = load('delta_vout_fase1_2.mat');
+% X-akse fra .csv fil (tid [s])
+t2 = data2.scope3.second;
+% Inputspænding fra .csv fil (Volt [V])
+vin2 = data2.scope3.Volt1;
+% Outputspænding fra .csv fil (Volt [V])
+vout2 = data2.scope3.Volt;
+% Antal samples i signalerne
+N2 = length(vin2);
+n2 = 0:N2-1;
+% Varighed af signalerne
+Tdur2 = t2(2000)-t2(1);
+% Samplevarighed
+Ts2 = Tdur2/N2;
+% Samplingfrekvens
+fs2 = 1/Ts2;
+% Midling af input og output
+vin_mid2 = medfilt1(vin2,200);
+vout_mid2 = medfilt1(vout2,200);
+% Max værdi for vin og vout
+vin_mid_max2 = max(vin_mid2);
+vout_mid_max2 = max(vout_mid2);
+
+% Plot af hele sigalet
+figure
+subplot(211)
+plot(n2*Ts2,vin_mid2,'b'), hold on
+plot(n2*Ts2,vout_mid2,'r')
+xlabel('Tid, [s]'), ylabel('Fase 1 & Vout, [V]')
+legend('Fase 1','Vout'), grid
+title('Fase 1 vs. Vout, 2. måling')
+
+% Udvælgelse af segmenter, hvor Fase 1 > Vout
+start = 640;
+slut = 955;
+vin_seg2 = vin_mid2(start:slut);
+vout_seg2 = vout_mid2(start:slut);
+% Plot af segmenter
+subplot(212)
+plot(n2(start:slut)*Ts2,vin_seg2), hold on
+plot(n2(start:slut)*Ts2,vout_seg2 )
+xlabel('Tid, [s]'), ylabel('Fase 1 & Vout, [V]')
+legend('Fase 1','Vout'), grid
+title('Segment af Vin og Vout')
+% Udregning af spændingstabet, (Vin - Vout)
+v_delta2 =mean(vin_seg2)-mean(vout_seg2);
+

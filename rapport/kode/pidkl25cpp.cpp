852c57c0e2c660032e9a989a39c6da0963ce8c76
unknown <simon.mylius.rasmussen@post.au.dk>
2019-05-28 23:12:54 +0200

    mangler konklusion

--- rapport/kode/pidkl25cpp.cpp
+++ rapport/kode/pidkl25cpp.cpp
@@ -7,14 +7,10 @@
 #include "fsl_debug_console.h"
 #include "rpm-detect.h"
 #include "servo_driver.h"
-//#include "pwm.h"
-// #include "FreeRTOS.h"
-// #include "task.h"
-// #include "queue.h"
 
 // Filtrering
 double alpha = 0.2;
-double measuredSpeed = 0 ;
+double measuredSpeed = 0;
 
 // Koefficienter
 double kp = 0.001;
@@ -44,57 +40,39 @@ double lastLowpassSpeed = 0;
 double lastOutput = 0;
 double lastLastMeasuredSpeed = 0;
 
-void velPID (int setpointSpeed, int measuredSpeed) {
-    lowpassSpeed = alpha * lastLowpassSpeed + (1-alpha ) * measuredSpeed;
-    K1 = kp * setpointWeight * (setpointSpeed-lastSetpointSpeed) + kp * (lastMeasuredSpeed-lowpassSpeed);
-    K2 = ki * (setpointSpeed-lowpassSpeed);
-    K3 = kd * (2 * lastMeasuredSpeed-lowpassSpeed-lastLastMeasuredSpeed);
-    output = lastOutput-K1-K2-K3;
-    if (output < 0) {
-      output = 0;
-    }
-    throttlePos = output/MAX_RPM;
-    lastLowpassSpeed = lowpassSpeed;
-    lastLastMeasuredSpeed = lastMeasuredSpeed;
-    lastMeasuredSpeed = lowpassSpeed;
-    lastSetpointSpeed = setpointSpeed;
-    lastOutput = output;
-    angle_throttle(throttlePos);
+void velPID(int setpointSpeed, int measuredSpeed) {
+	lowpassSpeed = alpha * lastLowpassSpeed + (1 - alpha) * measuredSpeed;
+	K1 = kp * setpointWeight * (setpointSpeed - lastSetpointSpeed)
+			+ kp * (lastMeasuredSpeed - lowpassSpeed);
+	K2 = ki * (setpointSpeed - lowpassSpeed);
+	K3 = kd * (2 * lastMeasuredSpeed - lowpassSpeed - lastLastMeasuredSpeed);
+	output = lastOutput - K1 - K2 - K3;
+	if (output < 0) {
+		output = 0;
+	}
+	throttlePos = output / MAX_RPM;
+	lastLowpassSpeed = lowpassSpeed;
+	lastLastMeasuredSpeed = lastMeasuredSpeed;
+	lastMeasuredSpeed = lowpassSpeed;
+	lastSetpointSpeed = setpointSpeed;
+	lastOutput = output;
+	angle_throttle(throttlePos);
 }
 
-// void setsp(void *p)
-// {
-//   while (1) {
-//     scanf("Ønsket hastighed(rpm) :%d", rpmch);
-//     vTaskDelay(200);
-//   }
-// }
-
-// void pid(void *p)
-// {
-//   while (1) {
-//     int measuredSpeed = get_rpm();
-//     velPID(rpmch, measuredSpeed);
-//     vTaskDelay(200);
-//   }
-// }
-
-init_read_rpm()
-
 int main(void) {
-  /* Init board hardware. */
-  BOARD_InitBootPins();
-  BOARD_InitBootClocks();
-  BOARD_InitBootPeripherals();
-  /* Init FSL debug console. */
-  BOARD_InitDebugConsole();
-  // init_read_rpm();
-  rpmch = 7000;
-  init_pwm();
-  start();
-  velPID(rpmch, measuredSpeed);
-  // xTaskCreate(setsp, "setsp", 1024, NULL, 1, NULL);
-  // xTaskCreate(pid, "pid", 1024, NULL, 2, NULL);
-  // vTaskStartScheduler();
-  return 0 ;
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+	// init_read_rpm();
+	rpmch = 7000;
+	init_read_rpm();
+	init_pwm();
+	while (1) {
+		int measure_rpm = get_rpm();
+		velPID(5000, measure_rpm); // set rpm to 5000 as setpoint
+		}
+	return 0;
 }
